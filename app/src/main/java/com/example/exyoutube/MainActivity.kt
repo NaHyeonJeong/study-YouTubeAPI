f529872c301cd7278b13f1d91b2cd9a7d09c04f5
NaHyeonJeong <pigismile@gmail.com>
2021-03-10 17:20:33 +0900

    영상 전체화면을 누르면 black 화면 되던거 수정 (AndroidManifest.xml에 android:configChanges 추가함)

--- app/src/main/java/com/example/exyoutube/MainActivity.kt
+++ app/src/main/java/com/example/exyoutube/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.exyoutube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : YouTubeBaseActivity() {
 
     companion object{
-        val VIDEO_ID: String = "YouTube Video Value"
-        val YOUTUBE_API_KEY: String = "API KEY"
+        val VIDEO_ID: String = "YouTube Video ID"
+        val YOUTUBE_API_KEY: String = "Your YouTube API KEY Value"
     }
 
     lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
@@ -34,7 +32,6 @@ class MainActivity : YouTubeBaseActivity() {
                 p2: Boolean
             ) {
                 youtubePlayer?.loadVideo(VIDEO_ID)
-
             }
 
             override fun onInitializationFailure(
@@ -42,12 +39,12 @@ class MainActivity : YouTubeBaseActivity() {
                 p1: YouTubeInitializationResult?
             ) {
                 Toast.makeText(applicationContext, "Something wrong", Toast.LENGTH_SHORT).show()
-
             }
 
         }
-        btnPlay.setOnClickListener(View.OnClickListener {view ->
+        btnPlay.setOnClickListener(View.OnClickListener { v ->
             youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
         })
     }
+
 }
